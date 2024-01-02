8090eb8351ec6a1bc2b01a9aa7e6f81546f05c8d
Shrutisd1003 <shrutisd1003@gmail.com>
2024-01-02 12:37:02 +0530

    commit

--- audio.py
+++ audio.py
@@ -1,19 +1,6 @@
-from gtts import gTTS
-import pygame
-import os
+import pyttsx3  
 
 def createAudio(text):
-    file_name = "welcome.mp3"
-
-    myobj = gTTS(text=text, lang="en", slow=False)
-    myobj.save(file_name)
-
-    pygame.mixer.init()
-    sound = pygame.mixer.Sound(file_name)
-    sound.play()
-
-    pygame.time.wait(int(sound.get_length() * 1000))
-
-    pygame.mixer.stop()
-
-    os.remove(file_name)
+    engine = pyttsx3.init()
+    engine.say(text) 
+    engine.runAndWait()
--- text.py
+++ text.py
@@ -2,14 +2,18 @@ import speech_recognition as sr
 
 def getText():
     recognizer = sr.Recognizer()
+
     print("Speak")
-    with sr.Microphone() as source:
-        recognizer.adjust_for_ambient_noise(source, duration=1)
-        recorded_audio = recognizer.listen(source, timeout=10)
-    print("Done recording")
-    text = recognizer.recognize_google(
-            recorded_audio, 
-            language="en-US")
+    try:
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source, duration=1)
+            recorded_audio = recognizer.listen(source, timeout=10)
+    except Exception as e:
+        print(f"Error: {e}")
+    else:
+        print("Done recording")
+    
+    text = recognizer.recognize_google(recorded_audio, language="en-US")
     
     return text
 
